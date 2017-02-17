c7bfed4ce431c9a57872661aeb6895f7ea893721
Christophe Brisbois <chr@brisbois.fr>
2017-02-18 00:34:42 +0100

    Add files via upload

--- dist/admin/log.php
+++ dist/admin/log.php
@@ -1,9 +1,11 @@
 <?php
 
   function write_log( $msg ) {
+    global $cfg_log;
+    
     if( $cfg_log === true ) {
       $time = @date('[d/M/Y:H:i:s]');
-      file_put_contents( 'admin/log.txt', '[' .$time . '] ' . $msg, FILE_APPEND );
+      file_put_contents( 'admin/log.txt', $time . ' ' . $msg . "\r\n", FILE_APPEND );
     }
   }
 
