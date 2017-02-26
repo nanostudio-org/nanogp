dc1feaae8493befccd54372a757be98d299a1117
Christophe Brisbois <chr@brisbois.fr>
2017-02-26 22:42:48 +0100

    Update config.php

--- dist/admin/config.php
+++ dist/admin/config.php
@@ -1,7 +1,6 @@
 <?php
 $cfg_client_id          = 'your Google API client id';
 $cfg_client_secret      = 'your Google API client secret';
-$cfg_application_name   = 'nanogallery2gp';
 $cfg_max_accounts       = 1;
 $cfg_log                = false;
 ?>
