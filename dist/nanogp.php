9a799456bb0f912cb24010bd2a98561c45e4c184
Christophe Brisbois <chr@brisbois.fr>
2017-02-26 01:16:25 +0100

    Add files via upload

--- dist/nanogp.php
+++ dist/nanogp.php
@@ -4,7 +4,7 @@
 * http://nanogallery2.nanostudio.org
 *
 * PHP 5.2+
-* @version    1.2.0
+* @version    1.2.1
 * @author     Christophe Brisbois - http://www.brisbois.fr/
 * @copyright  Copyright 2017
 * @license    GPLv3
@@ -51,13 +51,14 @@
   }
   
   // new query parameters
-  $nq = http_build_query($request);
+  // $nq = http_build_query($request);
 
   
   
   // ##### retrieve the list of albums
   if( $content_kind == 'album' ) {
-    $url = 'https://picasaweb.google.com/data/feed/api/user/' . $user_id . '?access_token=' . $atoken . '&' . $nq;
+    // $url = 'https://picasaweb.google.com/data/feed/api/user/' . $user_id . '?access_token=' . $atoken . '&' . $nq;
+    $url = 'https://picasaweb.google.com/data/feed/api/user/' . $user_id;
     // echo $url . PHP_EOL . '<br/>';    
 
     if( send_gprequest( $url ) === 'token_expired') {
@@ -70,7 +71,8 @@
   
   // ##### retrieve the content of one album
   if( $content_kind == 'photo' ) {
-    $url = 'https://picasaweb.google.com/data/feed/api/user/' . $user_id . '/albumid/' . $album_id . '?access_token=' . $atoken . '&' . $nq;
+    // $url = 'https://picasaweb.google.com/data/feed/api/user/' . $user_id . '/albumid/' . $album_id . '?access_token=' . $atoken . '&' . $nq;
+    $url = 'https://picasaweb.google.com/data/feed/api/user/' . $user_id . '/albumid/' . $album_id;
 
     if( send_gprequest( $url ) === 'token_expired') {
       // error -> get a new access token
@@ -82,20 +84,30 @@
   
   // ##### send the request to picasa/google photos
   function send_gprequest( $url ) {
-    global $callback, $atoken;
+    global $callback, $atoken, $request;
+
+    $request->access_token=$atoken;
     
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url . '&access_token=' . $atoken );
+    // curl_setopt($ch, CURLOPT_URL, $url . urlencode('&access_token=' . $atoken) );
+    $url = $url . '?' . http_build_query($request);
+    curl_setopt($ch, CURLOPT_URL, $url );
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array("GData-Version: 3"));
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_VERBOSE, true);
+
     $response = curl_exec($ch);
     $msg=curl_error($ch);
     $info = curl_getinfo($ch);
     curl_close($ch);
- 
+
+    if( $response == 'Token revoked' ) {
+      response_json( array('nano_status' => 'error', 'nano_message' => 'Token revoked - ' . $url ) );
+      exit;
+    }
+
     if( $response == 'No album found.' ) {
       response_json( array('nano_status' => 'error', 'nano_message' => 'No album found - ' . $url ) );
       exit;
@@ -110,7 +122,7 @@
       exit;
     }
     else {
-      response_json( array('nano_status' => 'error', 'nano_message' => 'curl error' . $msg . ' - ' . $url ) );
+      response_json( array('nano_status' => 'error', 'nano_message' => 'curl error' . $info['http_code'] . ' - ' . $msg . ' - ' . $url ) );
       exit;
     }
   
