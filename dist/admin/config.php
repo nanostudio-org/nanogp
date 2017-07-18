77d0e9aceb47645d061144226cb181400598fe50
Christophe Brisbois <chr@brisbois.fr>
2017-07-19 01:15:37 +0200

    Add files via upload

--- dist/admin/config.php
+++ dist/admin/config.php
@@ -3,4 +3,5 @@ $cfg_client_id          = 'your Google API client id';
 $cfg_client_secret      = 'your Google API client secret';
 $cfg_max_accounts       = 1;
 $cfg_log                = false;
+$albums_filter          = ['sauvegarde', 'backup'];
 ?>
